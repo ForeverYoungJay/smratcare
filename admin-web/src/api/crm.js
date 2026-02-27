afd8bd038c9fb7916a7d91a4e8b6c049f992511a
ForeverYoungJay <1301420427@qq.com>
2026-02-27 16:17:16 +0800

    fix: export getCrmLead in crm js api

--- admin-web/src/api/crm.js
+++ admin-web/src/api/crm.js
@@ -2,6 +2,9 @@ import request, { fetchPage } from '../utils/request';
 export function getCrmLeadPage(params) {
     return fetchPage('/api/crm/leads/page', params);
 }
+export function getCrmLead(id) {
+    return request.get(`/api/crm/leads/${id}`);
+}
 export function createCrmLead(data) {
     return request.post('/api/crm/leads', data);
 }
